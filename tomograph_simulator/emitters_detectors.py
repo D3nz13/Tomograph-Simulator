3349798cc0863636c62972fe97c8db8b31fc8aef
D3nz13 <seba.szczepan2@gmail.com>
2022-03-03 16:17:06 +0100

    Added elliptical shape initialization

--- tomograph_simulator/emitters_detectors.py
+++ tomograph_simulator/emitters_detectors.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 
 class EmittersDetectors:
-    def __init__(self, n: int, alpha: float, img_shape: tuple) -> None:
+    def __init__(self, n: int, alpha: float, img_shape: tuple, ellipse = True) -> None:
         """
         Args:
             n (int): number of emitters/detectors
@@ -20,11 +20,39 @@ class EmittersDetectors:
         self._width = w
         self._alpha = alpha
         self._radius = max(self._height//2, self._width//2)
-        self._emitters, self._detectors = self._initialize_positions()
+        
+        if ellipse:
+            self._emitters, self._detectors = self._initialize_positions_ellipse()
+        else:
+            self._emitters, self._detectors = self._initialize_positions_clipped_circle()
     
 
-    def _initialize_positions(self) -> tuple:
-        """Initializes emitters and detectors positions
+    def _initialize_positions_ellipse(self) -> tuple:
+        """Initializes emitters and detectors positions. They are formed into an elliptical shape
+
+        Returns:
+            tuple: emitters and detectors positions
+        """
+        middle_h = int(self._height/2)
+        middle_w = int(self._width/2)
+
+        angles = np.arange(0, self._num*self._alpha, self._alpha)
+
+        emitters_x = ((middle_w * np.cos(np.radians(angles))) + middle_w).astype(int)
+        emitters_y = ((middle_h * np.sin(np.radians(angles))) + middle_h).astype(int)
+
+        emitters_x = np.clip(emitters_x, 0, self._width - 1)
+        emitters_y = np.clip(emitters_y, 0, self._height - 1)
+
+        detectors_x = self._width - emitters_x
+        detectors_y = self._height - emitters_y
+
+        return np.stack((emitters_x, emitters_y), axis=-1), np.stack((detectors_x, detectors_y), axis=-1)
+
+
+    def _initialize_positions_clipped_circle(self) -> tuple:
+        """Initializes emitters and detectors positions. They are formed into an circular shape,
+        if it caused exceeding the image size, the position will be clipped to fit into.
 
         Returns:
             tuple: emitters and detectors positions
@@ -37,8 +65,8 @@ class EmittersDetectors:
         emitters_x = ((self._radius * np.cos(np.radians(angles))) + middle_w).astype(int)
         emitters_y = ((self._radius * np.sin(np.radians(angles))) + middle_h).astype(int)
 
-        emitters_x = np.clip(emitters_x, 0, self._width)
-        emitters_y = np.clip(emitters_y, 0, self._height)
+        emitters_x = np.clip(emitters_x, 0, self._width - 1)
+        emitters_y = np.clip(emitters_y, 0, self._height - 1)
 
         detectors_x = self._width - emitters_x
         detectors_y = self._height - emitters_y
@@ -46,14 +74,13 @@ class EmittersDetectors:
         return np.stack((emitters_x, emitters_y), axis=-1), np.stack((detectors_x, detectors_y), axis=-1)
 
 
-        
-
-
 if __name__ == "__main__":
-    emitter = EmittersDetectors(10, 5, (200, 200))
+    emitter = EmittersDetectors(72, 5, (100, 200))
     e_x, e_y = emitter._emitters[:, 0], emitter._emitters[:, 1]
     d_x, d_y = emitter._detectors[:, 0], emitter._detectors[:, 1]
     plt.scatter(e_x, e_y)
+    plt.xlim([0, 200])
+    plt.ylim([0, 200])
     plt.scatter(d_x, d_y)
     plt.show()
     
