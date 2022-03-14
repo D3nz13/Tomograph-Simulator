fa88a41873fbfa48494b6a892da04ba420e5a24d
Dominik Sawicki <dominik.sawicki52@gmail.com>
2022-03-14 17:44:17 +0100

    Update plots layout to columns

--- tomograph_simulator/gui.py
+++ tomograph_simulator/gui.py
@@ -4,7 +4,7 @@ import glob
 from emitters_detectors import EmittersDetectors
 import cv2
 import numpy as np
-from scipy.ndimage import rotate
+from helpers import make_circle
 
 st.write('# Tomograph Simulator')
 
@@ -26,23 +26,27 @@ if st.button('Run simulation', 'btn-1'):
     with st.spinner("Simulation is running..."):
         emitter = EmittersDetectors(num_of_emitters, alpha_angle, span, iterations, image=cv2.imread(filename, cv2.IMREAD_GRAYSCALE))
         sinogram = emitter.create_sinogram()
+
+    col1, col2 = st.columns(2)
+
     fig, ax = plt.subplots()
     ax.imshow(sinogram, cmap="gray")
-    st.write('## Sinogram')
-    st.pyplot(fig)
+    col1.write('## Sinogram')
+    col1.pyplot(fig)
     
     fft_sinogram = np.fft.fft(sinogram)
     fig2, ax2 = plt.subplots()
     ax2.imshow(fft_sinogram.real, cmap="gray")
-    st.write('## Fourier Transform Sinogram')
-    st.pyplot(fig2)
+    col2.write('## Fourier Sinogram')
+    col2.pyplot(fig2)
     
-    diameter = len(fft_sinogram[0])
-    circle_angle = len(fft_sinogram)
-    xx, yy = np.mgrid[:diameter, :diameter]
-    circle = (xx - diameter//2)**2 + (yy - diameter//2)**2
+    circle = make_circle(fft_sinogram, alpha_angle)
     fig3, ax3 = plt.subplots()
-    rotated = rotate(circle, angle=180//circle_angle, reshape=False)
-    ax3.imshow(rotated, cmap="gray")
+    ax3.imshow(circle.real, cmap="gray")
     st.write('## Circle')
     st.pyplot(fig3)
+    # diameter = len(fft_sinogram[0])
+    # circle_angle = len(fft_sinogram)
+    # xx, yy = np.mgrid[:diameter, :diameter]
+    # circle = (xx - diameter//2)**2 + (yy - diameter//2)**2
+    # rotated = rotate(circle, angle=180//circle_angle, reshape=False)
