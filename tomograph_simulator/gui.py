1dc16e89b61b8c90d37a84bd746685f65b8fd4bc
D3nz13 <seba.szczepan2@gmail.com>
2022-03-22 00:20:00 +0100

    Adjusted variable ranges, added plots for a base file and a specific reconstruction iteration

--- tomograph_simulator/gui.py
+++ tomograph_simulator/gui.py
@@ -12,41 +12,55 @@ files = glob.glob('../images/*')
 
 filename = st.selectbox('Filename', files)
 
-num_of_emitters = st.number_input('Number of emitters', 1, 100, 50)
+num_of_emitters = st.number_input('Number of emitters', 1, 150, 50)
 
-alpha_angle = st.number_input('Angle', 1, 360, 6)
+alpha_angle = st.number_input('Angle', 1, 20, 1)
 
-span = st.number_input('Span', 1, 100, 30)
+span = st.number_input('Span', 1, 180, 15)
 
-iterations = st.number_input('Number of iterations', 1, 100, 60)
+full_rotation = st.checkbox("Full rotation (360 degrees)", True)
 
+if not full_rotation:
+    iterations = st.number_input('Number of iterations', 1, 100, 60)
+else:
+    iterations = int(360/alpha_angle)
+
+show_all_iterations = st.checkbox("Show all iterations", True)
+
+image = None
+sinogram = None
+reconstruction = None
+last_set_iterations = iterations
 
 if st.button('Run simulation', 'btn-1'):
-    sinogram = None
     with st.spinner("Simulation is running..."):
-        emitter = EmittersDetectors(num_of_emitters, alpha_angle, span, iterations, image=cv2.imread(filename, cv2.IMREAD_GRAYSCALE))
+        image = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+        emitter = EmittersDetectors(num_of_emitters, alpha_angle, span, iterations, image)
         sinogram = emitter.create_sinogram()
+        reconstruction = emitter._reverse_sinogram(sinogram)
 
-    col1, col2 = st.columns(2)
+if image is not None and sinogram is not None and reconstruction is not None:
+    col1, col2, col3 = st.columns(3)
 
     fig, ax = plt.subplots()
-    ax.imshow(sinogram, cmap="gray")
-    col1.write('## Sinogram')
+    ax.imshow(image, cmap="gray")
+    col1.write('## Base image')
     col1.pyplot(fig)
-    
-    fft_sinogram = np.fft.fft(sinogram)
+
     fig2, ax2 = plt.subplots()
-    ax2.imshow(fft_sinogram.real, cmap="gray")
-    col2.write('## Fourier Sinogram')
+    ax2.imshow(sinogram, cmap="gray")
+    col2.write('## Sinogram')
     col2.pyplot(fig2)
-    
-    circle = make_circle(fft_sinogram, alpha_angle)
-    fig3, ax3 = plt.subplots()
-    ax3.imshow(circle.real, cmap="gray")
-    st.write('## Circle')
-    st.pyplot(fig3)
-    # diameter = len(fft_sinogram[0])
-    # circle_angle = len(fft_sinogram)
-    # xx, yy = np.mgrid[:diameter, :diameter]
-    # circle = (xx - diameter//2)**2 + (yy - diameter//2)**2
-    # rotated = rotate(circle, angle=180//circle_angle, reshape=False)
+
+    if not show_all_iterations:
+        fig3, ax3 = plt.subplots()
+        ax3.imshow(reconstruction, cmap="gray")
+        col3.write('## Reconstruction Image')
+        col3.pyplot(fig3)
+    else:
+        display_iteration = st.slider("Iteration no.", 1, last_set_iterations, 1)
+        reconstruction_iteration = cv2.imread(f"../results/{display_iteration:03d}.jpg", cv2.IMREAD_GRAYSCALE)
+        fig3, ax3 = plt.subplots()
+        ax3.imshow(reconstruction_iteration, cmap="gray")
+        col3.write('## Reconstruction Image')
+        col3.pyplot(fig3)
